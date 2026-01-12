25f19da74b1e294468e813d41fc5741e01bf4149
Arshdeep Singh <arsh250827@gmail.com>
2026-01-13 05:25:05 +0530

    greatest of 3 number in python

---
+++ greatestof3.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+if(a>b and a>c) :
+    print("a is greatest num")
+elif(b>a and b>c):
+    print("b is greatest.")
+elif(c>a and c>b):
+    print("c is greatest")
